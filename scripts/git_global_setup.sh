4cb417dce890849ebbb9ed4f588d80c317b960ef
Gustav <sir.gustav.the.coder@gmail.com>
2018-08-12 20:18:46 +0200

    moved scripts folder to git

---
+++ scripts/git_global_setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+git config --global user.name "Gustav"
+git config --global user.email "sir.gustav.the.coder@gmail.com"
+git config --global core.editor vim 
---
+++ scripts/keymap.sh
@@ -0,0 +1,1 @@
+setxkbmap -layout us,se -option 'grp:alt_shift_toggle'
