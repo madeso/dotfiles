0ed797d0ee622e8955260e42aadb7b2b44e2670c
Gustav <sir.gustav.the.coder@gmail.com>
2018-08-14 11:25:56 +0200

    moved capslock is escape code to xinitrc instead

--- dotfiles.py
+++ dotfiles.py
@@ -11,6 +11,7 @@ def get_data():
     data.add_file('_minttyrc', '.minttyrc')
     data.add_file('gnome2', '.gtkrc-2.0')
     data.add_file('gnome3.ini', '.config/gtk-3.0/settings.ini')
+    data.add_file('xinitrc', '.xinitrc')
     data.add_dir(
         dotlib.Dir("custom_fonts", ".fonts")
           .file("materialdesignicons-webfont.ttf")
