081f2d5f472561bff06171bfaf1a7bdaedd550e7
Gustav <sir.gustav.the.coder@gmail.com>
2018-09-30 13:24:46 +0200

    restore capslock as escape when chaning keyboard language

---
+++ scripts/fix-capslock.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+xmodmap -e "clear lock"
+xmodmap -e "keysym Caps_Lock = Escape"
--- scripts/switch-kblayout.sh
+++ scripts/switch-kblayout.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 (setxkbmap -query | grep -q "layout:\s\+us") && setxkbmap se || setxkbmap us
-
+source fix-capslock.sh
