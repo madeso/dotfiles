74f5120639027dcef88636de441266d7d3488470
Gustav <sir.gustav.the.coder@gmail.com>
2019-01-06 10:43:42 +0100

    simple script to git clone all aur repos that I need

---
+++ install_aur.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+git clone https://aur.archlinux.org/aseprite.git
+git clone https://aur.archlinux.org/chromium-widevine.git
+git clone https://aur.archlinux.org/clion.git
+git clone https://aur.archlinux.org/dropbox.git
+git clone https://aur.archlinux.org/godot.git
+git clone https://aur.archlinux.org/snapd.git
+git clone https://aur.archlinux.org/spotify.git
