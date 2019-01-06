999be9773b10bfac8fa1733108f4fa8e7196ebd3
Gustav <sir.gustav.the.coder@gmail.com>
2019-01-06 10:24:29 +0100

    added rofi function with callbacks

--- rofi.py
+++ rofi.py
@@ -3,15 +3,26 @@
 import subprocess
 import typing
 
-def rofi(input) -> typing.Optional[str]:
+def show(input) -> typing.Optional[str]:
    v = subprocess.run(['rofi', '-dmenu'], stdout=subprocess.PIPE, input='\n'.join(input), encoding='utf8')
    if v.returncode == 0:
        return v.stdout.strip()
    else:
        return None
 
-def test():
-   print(rofi(['tabbed', 'stacking']))
+def test_show():
+   print(show(['tabbed', 'stacking']))
+
+def select(input):
+    strings = []
+    for name in input:
+        strings.append(name)
+    sel = show(strings)
+    if sel is not None:
+        input[sel]()
+
+def test_select():
+    select({'tabbed': lambda: print('tabby cat'), 'stacked': lambda: print('stacking boxes')})
 
 if __name__ == "__main__":
-   test()
+    test_select()
