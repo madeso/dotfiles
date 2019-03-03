bfd46599f5caaaf19f86fcb465a08aba715a7a71
Gustav <sir.gustav.the.coder@gmail.com>
2019-03-03 15:49:57 +0100

    aur tool can check git status

--- aur.py
+++ aur.py
@@ -25,13 +25,40 @@ def find_git_folders(aur):
     return r
 
 
+def cmd(cmd, cwd):
+    return subprocess.check_output(cmd, cwd=cwd).decode('utf8').strip()
+
+def git_local(cwd):
+    return cmd(['git', 'rev-parse', '@'], cwd)
+
+def git_remote(cwd):
+    return cmd(['git', 'rev-parse', '@{u}'], cwd)
+
+def git_base(cwd):
+    return cmd(['git', 'merge-base', '@', '@{u}'], cwd)
+
+def git_info(cwd):
+    local = git_local(cwd)
+    remote = git_remote(cwd)
+    base = git_base(cwd)
+    if local == remote:
+        return 'Up to date'
+    elif local == base:
+        return 'Need to pull'
+    elif remote == base:
+        return 'Need to push'
+    else:
+        return 'Diverged'
+
+#############################################################################
+
 def handle_ls(args):
     aur = aur_path()
     print(aur)
 
     projects = find_git_folders(aur)
     for p in projects:
-        print(p)
+        print(p, git_info(p))
 
 
 def main():
