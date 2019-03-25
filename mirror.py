5deaa26ba7550087945b8834b346f787e2442ca4
Gustav <sir.gustav.the.coder@gmail.com>
2019-03-25 09:28:44 +0100

    started on mirror script

---
+++ mirror.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import filecmp
+import subprocess
+import shutil
+import sys
+import platform
+import typing
+import json
+import re
+from enum import Enum
+import urllib.request
+
+
+# src: https://stackoverflow.com/questions/7243750/download-file-from-web-in-python-3
+def download(url):
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    text = data.decode('utf-8')
+    return text
+
+
+def get_mirrors():
+    lines = download('https://www.archlinux.org/mirrorlist/?ip_version=6').splitlines()
+    country = []
+    name = ''
+    mirrors = {}
+    for l in lines:
+        if l.strip().startswith('##'):
+            if name!='' and len(country) > 0:
+                mirrors[name] = country
+                country = []
+            name = l.strip()[2:].strip().lower()
+        elif l.strip().startswith('#Server'):
+            country.append(l.strip()[1:])
+        elif len(l.strip()) == 0:
+            pass
+        else:
+            print('Unknown line', l)
+    if name=='' and len(country) > 0:
+        mirrors[name] = country
+        country = []
+    return mirrors
+
+
+def handle_ls(args):
+    mirrors = get_mirrors()
+    for m in mirrors.keys():
+        print(m)
+
+
+def handle_write(args):
+    mirrors = get_mirrors()
+    for l in args.lib:
+        if not l.lower() in mirrors:
+            print('no country named ', l)
+            return
+
+        print('#', l)
+        for c in mirrors[l.lower()]:
+            print(c)
+        print()
+
+
+def main():
+    parser = argparse.ArgumentParser(description='aur helper tool')
+    sub_parsers = parser.add_subparsers(dest='command_name', title='Commands', help='', metavar='<command>')
+
+    sub = sub_parsers.add_parser('ls', help='list countries')
+    sub.set_defaults(func=handle_ls)
+
+    sub = sub_parsers.add_parser('write', help='write mirrows')
+    sub.add_argument('lib', nargs='+', help='libraries to write')
+    sub.set_defaults(func=handle_write)
+
+    args = parser.parse_args()
+    if args.command_name is not None:
+        args.func(args)
+    else:
+        parser.print_help()
+
+
+if __name__ == "__main__":
+    main()
+
