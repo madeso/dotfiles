030ec0f6fbd815dfb886387fb2d1db2e71b9830d
Gustav <sir.gustav.the.coder@gmail.com>
2019-07-24 18:32:55 +0200

    added icons to rofi

--- rofi.py
+++ rofi.py
@@ -10,8 +10,10 @@ def show(input) -> typing.Optional[str]:
    else:
        return None
 
+
 def test_show():
    print(show(['tabbed', 'stacking']))
+
 
 def select(input):
     strings = []
@@ -21,8 +23,10 @@ def select(input):
     if sel is not None:
         input[sel]()
 
+
 def test_select():
     select({'tabbed': lambda: print('tabby cat'), 'stacked': lambda: print('stacking boxes')})
+
 
 def cmd(input):
     l = {}
@@ -32,8 +36,24 @@ def cmd(input):
         l[name] = create_cmd(input[name])
     select(l)
 
+
 def test_cmd():
     cmd({'list': ['ls'], 'all': ['ls', '-a'], 'clear': ['clear']})
 
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(description='rofi.py cli test')
+    sub = parser.add_subparsers()
+    parser.set_defaults(func=None)
+    sub.add_parser('show').set_defaults(func=test_show)
+    sub.add_parser('select').set_defaults(func=test_select)
+    sub.add_parser('cmd').set_defaults(func=test_cmd)
+    args = parser.parse_args()
+    if args.func is None:
+        parser.print_help()
+    else:
+        args.func()
+
 if __name__ == "__main__":
-    test_cmd()
+    main()
