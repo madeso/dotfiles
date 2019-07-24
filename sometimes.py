2d829e7092219d8115544b03a171f410cbfaf0f1
Gustav <sir.gustav.the.coder@gmail.com>
2019-07-24 19:41:14 +0200

    added helper script to run things from time to time

---
+++ sometimes.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import typing
+import json
+import datetime
+import re
+
+
+def file_exist(file: str) -> bool:
+    return os.path.isfile(file)
+
+
+def get_home_folder() -> str:
+    return os.path.expanduser('~')
+
+
+def get_config_file_name() -> str:
+    return os.path.join(get_home_folder(), '.sometimes.json')
+
+
+def get_user_data() -> typing.Dict[str, str]:
+    if file_exist(get_config_file_name()):
+        with open(get_config_file_name(), 'r') as f:
+            return json.loads(f.read())
+    else:
+        return {}
+
+
+def set_user_data(data: typing.Dict[str, str]):
+    with open(get_config_file_name(), 'w') as f:
+        print(json.dumps(data, sort_keys=True, indent=4), file=f)
+
+
+# from: https://stackoverflow.com/questions/4628122/how-to-construct-a-timedelta-object-from-a-simple-string
+regex = re.compile(r'((?P<hours>\d+?)hr)?((?P<minutes>\d+?)m)?((?P<seconds>\d+?)s)?')
+def parse_time(time_str):
+    parts = regex.match(time_str)
+    if not parts:
+        print('time string is not valid')
+        return None
+    parts = parts.groupdict()
+    time_params = {}
+    for (name, param) in parts.items():
+        if param:
+            time_params[name] = int(param)
+    return datetime.timedelta(**time_params)
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Manage my dot files.')
+    parser.add_argument('app')
+    parser.add_argument('timespan')
+    args = parser.parse_args()
+    data = get_user_data()
+    delta = parse_time(args.timespan)
+    if delta is None:
+        return
+    if args.app in data:
+        then = datetime.datetime.fromisoformat(data[args.app])
+        if then + delta < datetime.datetime.now():
+            data[args.app] = datetime.datetime.now().isoformat()
+            set_user_data(data)
+            print('passed')
+        else:
+            print('rejected')
+    else:
+        data[args.app] = datetime.datetime.now().isoformat()
+        set_user_data(data)
+        print('passed')
+
+
+if __name__ == "__main__":
+    main()
+
