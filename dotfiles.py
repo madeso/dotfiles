144bc02dbd79ce5cda1929170ed5b2566e9db726
Gustav <sir.gustav.the.coder@gmail.com>
2019-07-30 12:05:03 +0200

    xprofile to expose dotfiles/script to path in desktop land

--- dotfiles.py
+++ dotfiles.py
@@ -68,6 +68,7 @@ def get_data():
     data.add_file('gnome2', '.gtkrc-2.0')
     data.add_file('gnome3.ini', '.config/gtk-3.0/settings.ini')
     data.add_file('xinitrc', '.xinitrc')
+    data.add_file('xprofile', '.xprofile')
     data.add_dir(
         dotlib.Dir("custom_fonts", ".fonts")
           .file("materialdesignicons-webfont.ttf")
