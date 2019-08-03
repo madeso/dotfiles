384ee218cbfd44847c6e594ae26d3b5c4d08a02b
Gustav <sir.gustav.the.coder@gmail.com>
2019-08-03 08:54:58 +0200

    refactoring, and added the notion of critical updates, and started using critical updates of 25

--- i3blocks-scripts/arch-update.py
+++ i3blocks-scripts/arch-update.py
@@ -4,55 +4,14 @@
 # Licensed under the terms of the GNU GPL v3 only.
 #
 # i3blocks blocklet script to see the available updates of pacman and the AUR
+# modified by Gustav
+
 import subprocess
-from subprocess import check_output
 import argparse
-import re
-
-
-def create_argparse():
-    parser = argparse.ArgumentParser(description='Check for pacman updates')
-    parser.add_argument(
-        '-B',
-        '--base_bg',
-        default='white',
-        help='base background of the output(default=white)'
-    )
-    parser.add_argument(
-        '-U',
-        '--updates_available_bg',
-        default='white',
-        help='color of the background, when updates are available(default=white)'
-    )
-    parser.add_argument(
-        '-b',
-        '--base_color',
-        default='green',
-        help='base color of the output(default=green)'
-    )
-    parser.add_argument(
-        '-u',
-        '--updates_available_color',
-        default='yellow',
-        help='color of the output, when updates are available(default=yellow)'
-    )
-    parser.add_argument(
-        '-a',
-        '--aur',
-        action='store_true',
-        help='Include AUR packages. Attn: Yaourt must be installed'
-    )
-    parser.add_argument(
-        '-q',
-        '--quiet',
-        action = 'store_true',
-        help = 'Do not produce output when system is up to date'
-    )
-    return parser.parse_args()
 
 
 def get_updates():
-    output = check_output(['checkupdates']).decode('utf-8')
+    output = subprocess.check_output(['checkupdates']).decode('utf-8')
     if not output:
         return []
 
@@ -66,7 +25,7 @@ def get_updates():
 def get_aur_updates():
     output = ''
     try:
-        output = check_output(['yaourt', '-Qua']).decode('utf-8')
+        output = subprocess.check_output(['yaourt', '-Qua']).decode('utf-8')
     except subprocess.CalledProcessError as exc:
         # yaourt exits with 1 and no output if no updates are available.
         # we ignore this case and go on
@@ -82,23 +41,74 @@ def get_aur_updates():
     return aur_updates
 
 
+def collect_all_updates(also_aur):
+    updates = get_updates()
+    if also_aur:
+        updates += get_aur_updates()
+    return updates
+
+
 def message(bg, fg, text):
     print( "<span background='{0}' color='{1}'>{2}</span>".format(bg, fg, text))
 
 
 def main():
-    args = create_argparse()
+    parser = argparse.ArgumentParser(description='Check for pacman updates')
+    parser.add_argument(
+        '-B',
+        '--base_bg',
+        default='white',
+        help='base background of the output'
+    )
+    parser.add_argument(
+        '-U',
+        '--updates_available_bg',
+        default='red',
+        help='color of the background, when updates are available(default=white)'
+    )
+    parser.add_argument(
+        '-b',
+        '--base_color',
+        default='black',
+        help='base color of the text'
+    )
+    parser.add_argument(
+        '-u',
+        '--updates_available_color',
+        default='white',
+        help='color of the text, when updates are available'
+    )
+    parser.add_argument(
+        '-a',
+        '--aur',
+        action='store_true',
+        help='Include AUR packages. Attn: Yaourt must be installed'
+    )
+    parser.add_argument(
+        '-q',
+        '--quiet',
+        action = 'store_true',
+        help = 'Do not produce output when system is up to date'
+    )
+    parser.add_argument(
+        '-c',
+        '--critical_updates',
+        default=0,
+        type=int,
+        help='Number of updates available before color is switched'
+    )
+    args = parser.parse_args()
 
-    updates = get_updates()
-    if args.aur:
-        updates += get_aur_updates()
-    update_count = len(updates)
+    number_of_updates = len(collect_all_updates(args.aur))
 
-    if update_count > 0:
-        info = str(update_count) + ' updates available'
-        message(args.updates_available_bg, args.updates_available_color, info)
+    critical_updates = number_of_updates > args.critical_updates
+    bg = args.updates_available_bg    if critical_updates else args.base_bg
+    fg = args.updates_available_color if critical_updates else args.base_color
+
+    if number_of_updates > 0:
+        message(bg, fg, '{} updates available'.format(number_of_updates))
     elif not args.quiet:
-        message(args.base_bg, args.base_color, 'system up to date')
+        message(bg, fg, 'system up to date')
 
 
 if __name__ == "__main__":
