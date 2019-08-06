c86c3bc461a9a0d2dafc760fbf19f3a318c57ef9
Gustav <sir.gustav.the.coder@gmail.com>
2019-08-06 22:44:32 +0200

    notes about multimonitor setup

--- 1.install-and-enable.sh
+++ 1.install-and-enable.sh
@@ -4,9 +4,23 @@
 #
 # Run this file with sudo
 # it will enable and setup system
+#
+# note: header comments are indented with 3 spaces
 
-# ntfs usb mounting
+
+#################################################################
+#    randr/multimonitor
+#
+# https://wiki.archlinux.org/index.php/Xrandr
+sudo pacman -S arandr autorandr
+systemctl enable autorandr.service
+# use arandr to setup monitors, and "autorandr --save profilename" to save profiles
+
+
+#################################################################
+#    ntfs usb mounting
 sudo pacman -S diskie ntfs-3g
+
 
 #################################################################
 #    Time synchronisation
