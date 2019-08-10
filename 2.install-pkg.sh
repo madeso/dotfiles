1251dc9bf52d509cf7463d4f1123caf71cd6ba0e
Gustav <sir.gustav.the.coder@gmail.com>
2019-08-10 17:44:43 +0200

    reverted to arch wiki script + added a link

--- 2.install-pkg.sh
+++ 2.install-pkg.sh
@@ -1,2 +1,3 @@
 #!/bin/zsh
-pacman -S --needed `comm -12 <(pacman -Slq | sort) <(sort pkglist.txt)`
+# src: https://wiki.archlinux.org/index.php/Pacman/Tips_and_tricks#Install_packages_from_a_list
+pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort pkglist.txt))
