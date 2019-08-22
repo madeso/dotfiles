6c1db89e863a5b2f014d18d3786987d350433d3a
Gustav <sir.gustav.the.coder@gmail.com>
2019-08-22 21:31:03 +0200

    different compiler supports, options to disable things

--- scripts/tcmake.py
+++ scripts/tcmake.py
@@ -13,24 +13,40 @@ import re
 from enum import Enum
 
 
-def setup_cmake(source, build, type):
+def setup_cmake(source, build, type, compiler):
     os.makedirs(build, exist_ok=True)
     # https://stackoverflow.com/questions/7724569/debug-vs-release-in-cmake
-    subprocess.run(['cmake', '-G', 'Ninja', '-DCMAKE_BUILD_TYPE='+type, source], cwd=build)
+    subprocess.run(['cmake', '-G', 'Ninja',
+        '-D', 'CMAKE_C_COMPILER='+compiler,
+        '-D', 'CMAKE_CXX_COMPILER='+compiler,
+        '-DCMAKE_BUILD_TYPE='+type, source], cwd=build)
 
 def handle_setup(args):
     wd = os.getcwd()
     build = os.path.join(wd, 'build')
-    setup_cmake(wd, os.path.join(build, 'debug'), 'Debug')
-    setup_cmake(wd, os.path.join(build, 'release'), 'Release')
+    compilers = []
+    if args.clang:
+        compilers.append('clang')
+    if args.gcc:
+        compilers.append('gcc')
+    for compiler in compilers:
+        extra = '' if len(compilers)==1 else '-' + compiler
+        if args.debug:
+            setup_cmake(wd, os.path.join(build, 'debug'+extra), 'Debug', compiler)
+        if args.release:
+            setup_cmake(wd, os.path.join(build, 'release'+extra), 'Release', compiler)
     pass
 
 
 def main():
-    parser = argparse.ArgumentParser(description='cmake helper tool')
+    parser = argparse.ArgumentParser(description='Opinionated cmake helper tool')
     sub_parsers = parser.add_subparsers(dest='command_name', title='Commands', help='', metavar='<command>')
 
     sub = sub_parsers.add_parser('setup', help='setup the build folders')
+    sub.add_argument('--nogcc', dest='gcc', action='store_false', help="Don't setup gcc as a compiler")
+    sub.add_argument('--noclang', dest='clang', action='store_false', help="Don't setup clang as a compiler")
+    sub.add_argument('--nodebug', dest='debug', action='store_false', help="Don't setup a debug build")
+    sub.add_argument('--norelease', dest='release', action='store_false', help="Don't setup a release build")
     sub.set_defaults(func=handle_setup)
 
     args = parser.parse_args()
