0046086449e116e36e68000b5926e23ab35613c0
Gustav <sir.gustav.the.coder@gmail.com>
2019-08-26 22:49:42 +0200

    install pystache too

--- 2.install-pkg.sh
+++ 2.install-pkg.sh
@@ -1,3 +1,4 @@
 #!/bin/zsh
 # src: https://wiki.archlinux.org/index.php/Pacman/Tips_and_tricks#Install_packages_from_a_list
 pacman -S --needed $(comm -12 <(pacman -Slq | sort) <(sort pkglist.txt))
+pip install pystache
