6633dafdc833cc260df31fb1c3fd0773a6d37267
Gustav <sir.gustav.the.coder@gmail.com>
2020-03-18 20:46:17 +0100

    moved capslock and capslock setup to tools and made i3 use the new paths

--- tools/switch-kblayout.sh
+++ tools/switch-kblayout.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+pwd >> ~/capslock.txt
 (setxkbmap -query | grep -q "layout:\s\+us") && setxkbmap se || setxkbmap us
-source fix-capslock.sh
+source ~/dev/dotfiles/tools/setup_capslock.sh
 pkill -RTMIN+10 i3blocks
