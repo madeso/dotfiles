48473b09208743c599384c1144563beeaa2003c9
Gustav <sir.gustav.the.coder@gmail.com>
2020-06-06 18:31:30 +0200

    added ranger config

--- dotfiles.py
+++ dotfiles.py
@@ -78,6 +78,7 @@ def get_data():
     data.add_file(zsh, 'zshrc', '.zshrc')
     data.add_file(arch, 'xresources', '.Xresources')
     data.add_file(arch, 'powermate.toml', '.config/powermate.toml')
+    data.add_file(arch, 'ranger.conf', '.config/ranger/rc.conf')
     data.add_generated_file(arch, 'termite.conf', '.config/termite/config')
     data.add_file(arch, 'fonts.conf', '.config/fontconfig/fonts.conf')
     data.add_generated_file(arch, 'i3config', '.config/i3/config')
