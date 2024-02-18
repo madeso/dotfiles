3fa96eb50f926ef51b35da318def0d9359ebecdc
Gustav <sir.gustav.the.coder@gmail.com>
2024-02-18 14:24:47 +0100

    Add prefix command to rename

--- scripts/rename.py
+++ scripts/rename.py
@@ -25,6 +25,23 @@ def handle_lower(args):
         os.rename(from_path, new_path)
 
 
+def handle_add_prefix(args):
+    for relative in args.files:
+        from_path = os.path.abspath(os.path.realpath(relative))
+        pre, ext = os.path.splitext(from_path)
+        head, tail = os.path.split(pre)
+        if args.remove:
+            if tail.startswith(args.prefix):
+                tail = tail[len(args.prefix):]
+        else:
+            tail = args.prefix + tail
+        new_path = os.path.join(head, tail + ext)
+        if args.run:
+            os.rename(from_path, new_path)
+        else:
+            print(new_path)
+
+
 def main():
     parser = argparse.ArgumentParser(description='rename files')
     sub_parsers = parser.add_subparsers(dest='command_name', title='Commands', help='', metavar='<command>')
@@ -37,6 +54,13 @@ def main():
     sub = sub_parsers.add_parser('lower', help='change filename to small caps')
     sub.add_argument('files', nargs='+', help="the files to change")
     sub.set_defaults(func=handle_lower)
+
+    sub = sub_parsers.add_parser('add-prefix', help='add prefix to files')
+    sub.add_argument('prefix', help="the prefix to add")
+    sub.add_argument('--remove', action="store_true", help="remove instead of add")
+    sub.add_argument('--run', action="store_true", help="do the command instead of printing the new name")
+    sub.add_argument('files', nargs='+', help="the files to change")
+    sub.set_defaults(func=handle_add_prefix)
 
     args = parser.parse_args()
     if args.command_name is not None:
